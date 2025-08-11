004a1f2b47ae1cf41ea03dc6fc8d72539d0fe9d2
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-11 15:27:10 +0600

    added comments in dart

---
+++ 04_comments.dart
@@ -0,0 +1,13 @@
+void main() {
+  // This is a single-line comment
+
+  /*
+   This is a multi-line comment
+   that spans multiple lines
+  */
+
+  /// This is a documentation comment
+  /// for the main function.
+  /// It provides information about the function's purpose.
+  print('Hello, World!');
+}
