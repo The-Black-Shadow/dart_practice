626c5bd4ca19c4484d975c473fc724b2ba71c340
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-11 19:41:44 +0600

    added switch case

---
+++ 10_switch_case.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Switch Case in Dart
+  const weather = "rainy";
+
+  switch (weather) {
+    case "sunny":
+      print("Its a sunny day. Put sunscreen.");
+      break;
+    case "snowy":
+      print("Get your skis.");
+      break;
+    case "cloudy":
+    case "rainy":
+      print("Please bring umbrella.");
+      break;
+    default:
+      print("Sorry I am not familiar with such weather.");
+      break;
+  }
+}
---
+++ 11_enum_in_switch.dart
@@ -0,0 +1,21 @@
+//Use enum for better switch case handling
+// define enum outside main function
+enum Weather { sunny, snowy, cloudy, rainy }
+
+// main method
+void main() {
+  const weather = Weather.cloudy;
+
+  switch (weather) {
+    case Weather.sunny:
+      print("Its a sunny day. Put sunscreen.");
+      break;
+    case Weather.snowy:
+      print("Get your skis.");
+      break;
+    case Weather.rainy:
+    case Weather.cloudy:
+      print("Please bring umbrella.");
+      break;
+  }
+}
