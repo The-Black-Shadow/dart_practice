68542b74acc05552844c8cccd839037217c4b481
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-11 23:05:26 +0600

    added name parameter

--- 16_functions.dart
+++ 16_functions.dart
@@ -23,10 +23,17 @@ void greetings() {
   print("Hello World!!!");
 }
 
+//named parameters
+void displayInfo({required String name, required int age}) {
+  print("Name: $name");
+  print("Age: $age");
+}
+
 void main() {
   var total = add(2, 3);
   print("Total sum: $total");
   mul(2, 3);
+  displayInfo(name: "Mehedi", age: 25);
   var greeting = greet();
   print("Greeting: $greeting");
   greetings();
