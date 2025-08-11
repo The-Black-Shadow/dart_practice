49fe45d3a6b5f977f81a529089e13b8b5905de9f
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-11 13:03:54 +0600

    added dart basic and variable file

---
+++ 01_basic.dart
@@ -0,0 +1,6 @@
+void main() {
+  print("Hello World !");
+  var firstName = "Mehedi";
+  var lastName = "Hasan";
+  print("My name is $firstName $lastName");
+}
---
+++ 02_variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "Md Mehedi Hasan";
+  int age = 25;
+  double height = 5.4;
+  //num can be both int and double
+  num weight = 64.5;
+  bool isStudent = true;
+
+  print(
+    "Name: $name Age: $age, Height: $height, Weight: $weight, Is Student: $isStudent",
+  );
+}
